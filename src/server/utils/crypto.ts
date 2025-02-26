23671f19575cf11910ee30678920f22e6d242cbc
aiden <kyawmyo.h@7peakssoftware.com>
2025-02-26 13:58:06 +0700

    fix encrypt missleading code

--- src/server/utils/crypto.ts
+++ src/server/utils/crypto.ts
@@ -4,19 +4,11 @@ import { env } from 'process';
 const SECRET = env.AUTH_SECRET ?? "";
 
 export const encrypt = (data: string) => {
-  try {
-    return CryptoJS.AES.decrypt(data, SECRET).toString();
-  } catch {
-    throw new Error('Encryption failed.');
-  }
+  return CryptoJS.AES.encrypt(data, SECRET).toString();
 }
 
 export const decrypt = (data: string) => {
-  try {
-    return CryptoJS.AES.decrypt(data, SECRET).toString();
-  } catch {
-    throw new Error('Encryption failed');
-  }
+  return CryptoJS.AES.decrypt(data, SECRET).toString();
 }
 
 export const compare = (dataInput: string, dataCompare: string) => {
